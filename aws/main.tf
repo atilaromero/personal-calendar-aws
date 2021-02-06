fef99668c8abfc4c7bef06a14babfa604f97a1b9
Atila Romero <atilaromero@gmail.com>
2021-02-06 13:19:41 -0300

    Add some comments

--- aws/main.tf
+++ aws/main.tf
@@ -3,17 +3,19 @@ provider "aws" {
 }
 
 module "budget" {
+  # To use in free tier
+  # This module monitors the account costs.
+  # When it is greater than $0.01 per day, it sends an alert e-mail
   source = "./budget"
   subscriber_email_addresses = ["atilaromero@gmail.com"]
+}
+
+module "vpc" {
+  # Creates a VPC using 2 zones. Each zone has 2 subnets, one public and one private.
+  source = "./vpc"
 }
 
 module "dynamodb" {
   source = "./dynamodb"
   count = 0
 }
-
-module "vpc" {
-  source = "./vpc"
-  count = 1
-}
-
