23a8742fdbb498528d4eeda3ec32e4a0b256c110
Atila Romero <atilaromero@gmail.com>
2021-02-06 13:33:12 -0300

    include explicit config on main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "budget" {
   # To use in free tier
   # This module monitors the account costs.
-  # When it is greater than $0.01 per day, it sends an alert e-mail
+  # When it is greater than $0.01 per day, it sends an e-mail alert
   source = "./budget"
   subscriber_email_addresses = ["atilaromero@gmail.com"]
 }
@@ -13,6 +13,19 @@ module "budget" {
 module "vpc" {
   # Creates a VPC using 2 zones. Each zone has 2 subnets, one public and one private.
   source = "./vpc"
+  name  = "App"
+  cidr_block = "10.0.0.0/16"
+  region = "us-east-1"
+  subnet_1 = {
+    availability_zone = "us-east-1a"
+    public_CIDR = "10.0.1.0/24"
+    private_CIDR = "10.0.101.0/24"
+  }
+  subnet_2 = {
+    availability_zone = "us-east-1b"
+    public_CIDR = "10.0.2.0/24"
+    private_CIDR = "10.0.102.0/24"
+  }
 }
 
 module "dynamodb" {
