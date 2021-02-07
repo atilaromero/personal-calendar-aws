0618a851bfc5291a4e6390cb7b2827a4e4f980c4
Atila Romero <atilaromero@gmail.com>
2021-02-07 12:43:28 -0300

    move remote_tfstate to its own file

---
+++ aws/remote_tfstate.tf
@@ -0,0 +1,19 @@
+# Stores terraform.tfstate in aws
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-atilaromero-terraform-examples"
+    dynamodb_table = "terraform-locks"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+# Creates a S3 bucket and a DynamoDB with a LockID table
+# These are requirements to use a remote tfstate
+# bucket and dynamodb_table must match the same values above
+module s3_backend {
+    source         = "./s3_backend"
+    bucket         = "terraform-state-atilaromero-terraform-examples"
+    dynamodb_table = "terraform-locks"
+}
+
