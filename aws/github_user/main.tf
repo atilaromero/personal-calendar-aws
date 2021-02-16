a3a49f458176d794f60f43f476fa90fd05d2b8fd
Atila Romero <atilaromero@gmail.com>
2021-02-16 12:43:05 -0300

    fix permissions and keys

--- aws/github_user/main.tf
+++ aws/github_user/main.tf
@@ -10,52 +10,15 @@ resource "aws_iam_user" "github" {
   name = var.user
 }
 
-data "aws_iam_user" "github" {
-  user_name = aws_iam_user.github.name
-}
-
-output "AWS_ACCESS_KEY_ID" {
-  value = data.aws_iam_user.github.user_id
-}
-
-resource "aws_iam_access_key" "github" {
-  user = aws_iam_user.github.name
-}
-output "AWS_SECRET_ACCESS_KEY" {
-  value = aws_iam_access_key.github.secret
-}
-
 data "aws_iam_policy_document" "ecr" {
   statement {
-    actions   = ["ecr:PutImageTagMutability",
-                "ecr:StartImageScan",
-                "ecr:ListTagsForResource",
-                "ecr:UploadLayerPart",
-                "ecr:BatchDeleteImage",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:CompleteLayerUpload",
-                "ecr:TagResource",
-                "ecr:DescribeRepositories",
-                "ecr:DeleteRepositoryPolicy",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:ReplicateImage",
-                "ecr:GetLifecyclePolicy",
-                "ecr:PutLifecyclePolicy",
-                "ecr:DescribeImageScanFindings",
-                "ecr:GetLifecyclePolicyPreview",
-                "ecr:CreateRepository",
-                "ecr:PutImageScanningConfiguration",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:DeleteLifecyclePolicy",
-                "ecr:PutImage",
-                "ecr:UntagResource",
-                "ecr:BatchGetImage",
-                "ecr:DescribeImages",
-                "ecr:StartLifecyclePolicyPreview",
-                "ecr:InitiateLayerUpload",
-                "ecr:GetRepositoryPolicy"]
+    actions   = ["*"]
     resources = ["arn:aws:ecr:*:*:repository/${var.repository}"]
+  }
+
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
   }
 }
 
@@ -63,4 +26,14 @@ resource "aws_iam_user_policy" "github" {
   name = "ecr"
   user = aws_iam_user.github.name
   policy = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_access_key" "github" {
+  user = aws_iam_user.github.name
+}
+output "AWS_ACCESS_KEY_ID" {
+  value = aws_iam_access_key.github.id
+}
+output "AWS_SECRET_ACCESS_KEY" {
+  value = aws_iam_access_key.github.secret
 }
